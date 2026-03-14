f2f78d9d46dcf3ebc81aedef3224b36fb5df89e5
Subhashis <subhashisdhara123@gmail.com>
2026-03-14 13:23:24 +0530

    feat: add root route for service status check

--- src/app.js
+++ src/app.js
@@ -15,6 +15,10 @@ const transactionRoutes = require("./routes/transaction.routes")
 /**
  * - Use Routes
  */
+
+app.get("/", (req, res) => {
+    res.send("Bank Ledger Service is up and running")
+})
 app.use('/api/auth', authRouter);
 app.use("/api/accounts", accountRouter);
 app.use("/api/transactions", transactionRoutes)
